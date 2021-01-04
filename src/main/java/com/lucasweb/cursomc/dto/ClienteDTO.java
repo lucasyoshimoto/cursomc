141390aa9bba0d665c1ae0dc82cdd13ae362ed76
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-04 16:57:49 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/lucasweb/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/lucasweb/cursomc/dto/ClienteDTO.java
@@ -2,10 +2,10 @@ package com.lucasweb.cursomc.dto;
 
 import java.io.Serializable;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.lucasweb.cursomc.domain.Cliente;
 
