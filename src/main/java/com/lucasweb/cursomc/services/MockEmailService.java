77bc41eee7fcc67d74d7afc98e8219d67ea0ea16
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-06 10:51:06 -0300

    Email HTML

--- src/main/java/com/lucasweb/cursomc/services/MockEmailService.java
+++ src/main/java/com/lucasweb/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.lucasweb.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,5 +16,14 @@ public class MockEmailService extends AbstractEmailService {
 		lOG.info("Simulando envio de email...");
 		lOG.info(msg.toString());
 		lOG.info("Email enviado");
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		lOG.info("Simulando envio de email...");
+		lOG.info(msg.toString());
+		lOG.info("Email enviado");
+		
 	}
 }
