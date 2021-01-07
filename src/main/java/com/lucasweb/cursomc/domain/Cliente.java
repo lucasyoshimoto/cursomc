8d7c47dbfe790d979710489e6711760c34834c2d
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-07 15:22:13 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/lucasweb/cursomc/domain/Cliente.java
+++ src/main/java/com/lucasweb/cursomc/domain/Cliente.java
@@ -51,6 +51,8 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	private String imageUrl;
+	
 	public Cliente(){
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -169,5 +171,13 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 }
