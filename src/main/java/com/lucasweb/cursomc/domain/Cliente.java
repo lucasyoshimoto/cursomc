59772ff42b9b219ad07a8c0416fda14b97afa4f1
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-07 16:00:53 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/lucasweb/cursomc/domain/Cliente.java
+++ src/main/java/com/lucasweb/cursomc/domain/Cliente.java
@@ -51,8 +51,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
 	public Cliente(){
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -171,13 +169,5 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 }
